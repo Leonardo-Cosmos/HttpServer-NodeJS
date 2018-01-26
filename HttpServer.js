bc46228bb504c46f3dd44da8e50955223304e669
Leonardo_Cosmos <kylin_cl@hotmail.com>
2018-01-26 19:14:03 +0800

    Enhance: new sample

--- HttpServer.js
+++ HttpServer.js
@@ -1,18 +1,19 @@
 /* 2017/1/25 */
-var finalhandler = require('finalhandler');
-var http = require('http');
-var Router = require('router');
+const finalhandler = require('finalhandler');
+const http = require('http');
+const https = require('https');
+const Router = require('router');
 
-var bodySyncHandler = require('./Handler/RequestBodySyncHandler');
-var bodyAsyncHandler = require('./Handler/RequestBodyAsyncHandler');
-var urlSyncHandler = require('./Handler/RequestUrlSyncHandler');
-var urlAsyncHandler = require('./Handler/RequestUrlAsyncHandler');
+const httpHandler = require('./Handler/HttpHandler');
 
-var sampleUrlJson = require('./JsonService/SampleUrlJsonService')
-var sampleBodyJson = require('./JsonService/SampleBodyJsonService');
-var sampleSoap = require('./SoapService/SampleSoapService');
+const SampleConstJsonService = require('./JsonService/Impl/SampleConstJsonService');
+const SampleBodyJsonService = require('./JsonService/Impl/SampleBodyJsonService');
+const SampleUrlJsonService = require('./JsonService/Impl/SampleUrlJsonService');
 
-const port = 9010;
+const SampleSoapService = require('./SoapService/Impl/SampleSoapService');
+
+const httpPort = 32080;
+const httpsPort = 32443;
 const contentType = 'Content-Type';
 const plainTextUtf8 = 'text/plain; charset=utf-8';
 const jsonUtf8 = 'application/json; charset=utf-8';
@@ -21,28 +22,51 @@ const xmlUtf8 = 'text/xml; charset=utf-8';
 var router = Router();
 router.get('/', function(request, response) {
   response.setHeader(contentType, plainTextUtf8);
-  bodySyncHandler(request, response, (requestBody) => {
+  httpHandler.handleReqBodyResSync(request, response, (requestBody) => {
     return 'HTTP server';
   });
 });
 
-router.get('/json', function(request, response) {
-  response.setHeader(contentType, jsonUtf8);
-  urlAsyncHandler(request, response, sampleUrlJson);
-});
-
 router.post('/json', function(request, response) {
   response.setHeader(contentType, jsonUtf8);
-  bodyAsyncHandler(request, response, sampleBodyJson);
+  httpHandler.handleReqBodyResAsync(request, response, (requestBody, responseCallback) => {
+    var requestJson = JSON.parse(requestBody);
+    var responseJson = requestJson;
+    responseCallback(JSON.stringify(responseJson));
+  });
 });
 
-router.post('/soap', function(request, response) {
+let sampleConstJsonService = new SampleConstJsonService();
+router.get('/sample/json/const', function(request, response) {
+  response.setHeader(contentType, jsonUtf8);
+  httpHandler.handleConstReqAsyncRes(request, response, sampleConstJsonService);
+});
+
+let sampleBodyJsonService = new SampleBodyJsonService();
+router.post('/sample/json/body', function(request, response) {
+  response.setHeader(contentType, jsonUtf8);
+  httpHandler.handleReqBodyResAsync(request, response, sampleBodyJsonService);
+});
+
+let sampleUrlJsonService = new SampleUrlJsonService();
+router.get('/sample/json/url', function(request, response) {
+  response.setHeader(contentType, plainTextUtf8);
+  httpHandler.handleReqUrlResAsync(request, response, sampleUrlJsonService);
+});
+
+let sampleSoapService = new SampleSoapService();
+router.post('/sample/soap', function(request, response) {
   response.setHeader(contentType, xmlUtf8);
-  bodyAsyncHandler(request, response, sampleSoap);
+  dataAsyncHandler(request, response, sampleSoapService);
 });
 
 http.createServer(function(request, response) {
   router(request, response, finalhandler(request, response));
-}).listen(port);
+}).listen(httpPort);
 
-console.log("HTTP server is running on http://127.0.0.1:" + port + "/");
+https.createServer(function(request, response) {
+  router(request, response, finalhandler(request, response));
+}).listen(httpsPort);
+
+console.log(`HTTP server is running on http://127.0.0.1:${httpPort}/`);
+console.log(`HTTPS server is running on https://127.0.0.1:${httpsPort}/`);
